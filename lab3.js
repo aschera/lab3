dbe5537df1d9e601970ed91d2947318612744d2a
C.B <aschera@gmail.com>
2016-11-21 21:11:46 +0100

    Add files via upload

--- lab3.js
+++ lab3.js
@@ -1,4 +1,4 @@
-//Button to toggle menu
+//Toggle menu
 function toggleMenu() {
   var menuBox = document.getElementById('nav-trigger');    
   if(menuBox.style.display === "block") { 
@@ -11,6 +11,81 @@ function toggleMenu() {
     print('menu is shown!');
   }
 }
+
+//Circle menu and options.
+function showOne(showme) {
+    var element = document.getElementById(showme);
+    element.style.display = 'block';
+    print('Showing Circle options');
+    
+    
+    var c = document.getElementById("myCanvas");  
+        var mouseX1;
+        var mouseY1;
+        var mouseX2;
+        var mouseY2;
+        var radius = 0; 
+        var j = 0;
+            c.addEventListener("click", function(){
+                if(j<1){
+                    coords = c.relMouseCoords(event);
+                        mouseX1 = coords.x;
+                        mouseY1 = coords.y;
+                    j++;
+                    print('your centerpoint is: ' + mouseX1 + ':x,' + mouseY1 + ':y.');
+                }
+                else if((j<2) && (j=1)){
+                    coords = c.relMouseCoords(event);
+                        mouseX2 = coords.x;
+                        mouseY2 = coords.y;
+                    j++;
+                    radius = Math.sqrt( 
+			                 ((mouseX1 -mouseX2)*(mouseX1 -mouseX2)) + 
+			                 ((mouseY1-mouseY2)*(mouseY1-mouseY2))
+			                 );
+                    print('your radius is: ' + radius);
+                    draw(mouseX1,mouseY1,radius);
+                } 
+//draw
+function draw(x,y,radius) {
+    
+        this.x = x;
+        this.y = y;
+    
+      var canvas = document.getElementById('myCanvas');
+      var context = canvas.getContext('2d');
+
+      context.beginPath();
+      context.arc(this.x, this.y, radius, 0,2 * Math.PI, false);
+      context.fillStyle = chosenColor;
+      context.fill();
+      context.lineWidth = 1;
+      context.strokeStyle = '#003300';
+      context.stroke();
+  }}) }
+
+// coordinates
+function relMouseCoords(event){
+    var totalOffsetX = 0;
+    var totalOffsetY = 0;
+    var canvasX = 0;
+    var canvasY = 0;
+    var currentElement = this;
+
+    do{
+        totalOffsetX += currentElement.offsetLeft - currentElement.scrollLeft;
+        totalOffsetY += currentElement.offsetTop - currentElement.scrollTop;
+    }
+    while(currentElement = currentElement.offsetParent)
+
+    canvasX = event.pageX - totalOffsetX;
+    canvasY = event.pageY - totalOffsetY;
+
+    return {x:canvasX, y:canvasY}
+}
+HTMLCanvasElement.prototype.relMouseCoords = relMouseCoords;
+
+
 
 // status bar text function
 function print(text) {
@@ -29,6 +104,7 @@ function print(text) {
 }
 
 // color menu
+chosenColor ='';
 window.onload=function(){
 var e = document.getElementById("color-menu");
   var value = e.options[e.selectedIndex].value;
@@ -37,7 +113,9 @@ var e = document.getElementById("color-menu");
 let color = function(event) {
   if(e.options[e.selectedIndex].value !== "choice")
   print('Your Color:  ' + e.value);
+  chosenColor = e.options[e.selectedIndex].value;
 }
 e.addEventListener('click', color);
+
 }
 
