3978325dd29101aac4e72b3ecea6ed6746f59983
C.B <aschera@gmail.com>
2016-11-23 18:31:00 +0100

    Add files via upload

--- lab3.js
+++ lab3.js
@@ -12,9 +12,14 @@ function toggleMenu() {
   }
 }
 
+
+// ///////////////////////////////////////////////////////////////////////////////////////////////////
 //Circle menu and options.
-function showOne(showme) {
-    var element = document.getElementById(showme);
+function showOne() {
+    var e = document.getElementById('whatwedo1');
+    e.style.display = 'none';
+    
+    var element = document.getElementById('whatwedo');
     element.style.display = 'block';
     print('Showing Circle options');
     
@@ -44,9 +49,14 @@ function showOne(showme) {
 			                 ((mouseY1-mouseY2)*(mouseY1-mouseY2))
 			                 );
                     print('your radius is: ' + radius);
-                    draw(mouseX1,mouseY1,radius);
+                    
+                    var drawIt = document.getElementById('draw');
+                    function drawing() { print('Pressing Draw button.');print('Drawing your circle.');
+                        draw(mouseX1,mouseY1,radius);
+                    }
+                    drawIt.onclick = drawing;
                 } 
-//draw
+//draw circle
 function draw(x,y,radius) {
     
         this.x = x;
@@ -59,10 +69,77 @@ function draw(x,y,radius) {
       context.arc(this.x, this.y, radius, 0,2 * Math.PI, false);
       context.fillStyle = chosenColor;
       context.fill();
-      context.lineWidth = 1;
-      context.strokeStyle = '#003300';
+      context.lineWidth = 5;
+      context.strokeStyle = lineColor;
       context.stroke();
-  }}) }
+      
+  }}) 
+}
+// ///////////////////////////////////////////////////////////////////////////////////////////////////
+
+// ///////////////////////////////////////////////////////////////////////////////////////////////////
+//Triangle menu and options.
+function showTwo() {
+    var e = document.getElementById('whatwedo');
+    e.style.display = 'none';
+    
+    var element = document.getElementById('whatwedo1');
+    element.style.display = 'block';
+    print('Showing Triangle options');
+    
+    
+    var c = document.getElementById("myCanvas");  
+        var mouseX1;
+        var mouseY1;
+        var mouseX2;
+        var mouseY2;
+        var radius = 0; 
+        var j = 0;
+            c.addEventListener("click", function(){
+                if(j<1){
+                    coords = c.relMouseCoords(event);
+                        mouseX1 = coords.x;
+                        mouseY1 = coords.y;
+                    j++;
+                    print('your centerpoint is: ' + mouseX1 + ':x,' + mouseY1 + ':y.');
+                }
+                else if((j<2) && (j=1)){
+                    coords = c.relMouseCoords(event);
+                        mouseX2 = coords.x;
+                        mouseY2 = coords.y;
+                    j++;
+                    radius = Math.sqrt( 
+			                 ((mouseX1 -mouseX2)*(mouseX1 -mouseX2)) + 
+			                 ((mouseY1-mouseY2)*(mouseY1-mouseY2))
+			                 );
+                    print('your radius is: ' + radius);
+                    
+                    var drawIt = document.getElementById('draw');
+                    function drawing() { print('Pressing Draw button.');print('Drawing your circle.');
+                        draw(mouseX1,mouseY1,radius);
+                    }
+                    drawIt.onclick = drawing;
+                } 
+//draw circle
+function draw(x,y,radius) {
+    
+        this.x = x;
+        this.y = y;
+    
+      var canvas = document.getElementById('myCanvas');
+      var context = canvas.getContext('2d');
+
+      context.beginPath();
+      context.arc(this.x, this.y, radius, 0,2 * Math.PI, false);
+      context.fillStyle = chosenColor;
+      context.fill();
+      context.lineWidth = 5;
+      context.strokeStyle = lineColor;
+      context.stroke();
+      
+  }}) 
+}
+// ///////////////////////////////////////////////////////////////////////////////////////////////////
 
 // coordinates
 function relMouseCoords(event){
@@ -93,8 +170,8 @@ function print(text) {
   let lista = con.getElementsByTagName('pre');
   if( lista.length === 0 ) {  
     let e = document.createElement('pre');
-    e.style.backgroundColor = 'black';
-    e.style.color = 'lightblue';
+    e.style.backgroundColor = 'lightblue';
+    e.style.color = 'black';
     e.innerText = text + '\n';
     con.appendChild(e);
   } else {
@@ -103,19 +180,19 @@ function print(text) {
   }
 }
 
-// color menu
-chosenColor ='';
-window.onload=function(){
-var e = document.getElementById("color-menu");
-  var value = e.options[e.selectedIndex].value;
-  var text = e.options[e.selectedIndex].text;
- console.log(e.options[e.selectedIndex].value);
-let color = function(event) {
-  if(e.options[e.selectedIndex].value !== "choice")
-  print('Your Color:  ' + e.value);
-  chosenColor = e.options[e.selectedIndex].value;
-}
-e.addEventListener('click', color);
 
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
